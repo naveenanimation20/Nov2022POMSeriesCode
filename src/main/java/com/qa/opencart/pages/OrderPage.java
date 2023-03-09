81b2a742add39cc6328d6a260d6b3e2b32b87ba7
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-03-09 09:09:39 +0530

    added order page

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class OrderPage {
+	
+	By loc = By.id("order");
+	
+	public void getOrder() {
+		System.out.println("get order");
+	}
+	
+	
+
+}
