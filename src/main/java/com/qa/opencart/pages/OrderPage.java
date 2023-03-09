5c2c6a54a7db6e2b8193caeb3d018cdc48dd0727
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-03-09 09:22:20 +0530

    updated order page as per Naveen's comments

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -5,9 +5,15 @@ import org.openqa.selenium.By;
 public class OrderPage {
 	
 	By loc = By.id("order");
+	By price = By.id("price");
+
 	
 	public void getOrder() {
 		System.out.println("get order");
+	}
+	
+	public void getPrice() {
+		System.out.println("get price");
 	}
 	
 	
