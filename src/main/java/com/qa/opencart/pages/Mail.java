ebe2ceb99d834bb509a598b4acc528851c9e74b9
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-03-13 09:12:53 +0530

    Create
    added mail file

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class Mail {
+	
+  public void sendMail(){
+    
+    System.out.println("mail sent");
+  
+  }
+	
+	
+
+}
