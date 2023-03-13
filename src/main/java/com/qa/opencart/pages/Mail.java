8a19dd6360937dc82daf6a09a9e694f870450def
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-03-13 09:17:08 +0530

    mail naveen

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class Mail {
+
+	public void sendMail() {
+
+		System.out.println("mail sent --- Naveen");
+
+	}
+
+	public void getMail() {
+		System.out.println("get mail -- Naveen");
+	}
+
+}
