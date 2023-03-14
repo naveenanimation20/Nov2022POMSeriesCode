1a1ba74431a913e00c8552c93320d49230de9c63
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-03-14 09:02:46 +0530

    updated fix for chrome 111.x

--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -20,6 +20,7 @@ public class OptionsManager {
 	
 	public ChromeOptions getChromeOptions() {
 		co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
 		if(Boolean.parseBoolean(prop.getProperty("headless").trim()))
 			{
 			System.out.println("=========Running chrome in headless==========");
