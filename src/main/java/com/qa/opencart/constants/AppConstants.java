5ad40fe5193cef284dcdb783e785f89904ccf28c
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-05-28 06:53:45 +0530

    Update AppConstants.java

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -9,7 +9,7 @@ public class AppConstants {
 	public static final int DEFAULT_SHORT_TIME_OUT = 5;
 	public static final int DEFAULT_LONG_TIME_OUT = 20;
 
-	public static final String LOGIN_PAGE_TITLE_VALUE = "Account Login";
+	public static final String LOGIN_PAGE_TITLE_VALUE = "Account Login11";
 	public static final String LOGIN_PAGE_URL_FRACTION_VALUE = "route=account/login";
 
 	public static final String ACCOUNTS_PAGE_TITLE_VALUE = "My Account";
